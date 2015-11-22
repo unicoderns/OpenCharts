33d2e79e12be80afe5abffbc4f245a4f14fcf139
Chriss Mejía <me@chrissmejia.com>
2015-11-22 04:54:07 -0600

    Grunt setup, ordering files, observers base

--- opencharts.js
+++ opencharts.js
@@ -29,9 +29,9 @@
 var opencharts = {
     _selector: "",
     _data: "",
-    _type: ""//,
-//    _settings: {}, // Settings for each chart
-}
+    _type: "",
+    _charts: {}, // Settings for each chart
+};
 
 //------------------------------------------------------------------------------------------
 // Select the chart
@@ -41,17 +41,7 @@ opencharts.select =  function(selector){
 
     this._selector = selector;
     return this;
-}
-
-//------------------------------------------------------------------------------------------
-// Set the data for the current chart
-//------------------------------------------------------------------------------------------
-opencharts.data =  function(data){
-    "use strict";
-
-    this._data = data;
-    return this;
-}
+};
 
 //------------------------------------------------------------------------------------------
 // Print the current object
@@ -61,7 +51,7 @@ opencharts.print =  function(){
 
     console.log(this);
     return this;
-}
+};
 
 //------------------------------------------------------------------------------------------
 // Init
@@ -69,10 +59,10 @@ opencharts.print =  function(){
 opencharts._init = function() {
     "use strict";
 
-}
+};
 
 //------------------------------------------------------------------------------------------
-// Getting data object from DOM using d3
+// Getting data object from DOM using d3 (custom tags)
 //------------------------------------------------------------------------------------------
 opencharts._getData = function(selector) {
     "use strict";
@@ -90,7 +80,7 @@ opencharts._getData = function(selector) {
     });
 
     return data;
-}
+};
 
 //------------------------------------------------------------------------------------------
 // On page load create custom-tags elements
@@ -105,6 +95,104 @@ document.addEventListener("DOMContentLoaded", function() {
         var selector = "#" + id;
         var data = opencharts._getData(selector);
 
-        opencharts.select(selector).data(data).pie().create(); // Create pie
+        opencharts.select(selector).pie().data(data).create(); // Create pie
     });
 });
+////////////////////////////////////////////////////////////////////////////////////////////
+// The MIT License (MIT)                                                                  //
+//                                                                                        //
+// Copyright (C) 2015  Christopher Mejía Montoya - me@chrissmejia.com - chrissmejia.com   //
+//                                                                                        //
+// Permission is hereby granted, free of charge, to any person obtaining a copy           //
+// of this software and associated documentation files (the "Software"), to deal          //
+// in the Software without restriction, including without limitation the rights           //
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell              //
+// copies of the Software, and to permit persons to whom the Software is                  //
+// furnished to do so, subject to the following conditions:                               //
+//                                                                                        //
+// The above copyright notice and this permission notice shall be included in all         //
+// copies or substantial portions of the Software.                                        //
+//                                                                                        //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR             //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,               //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE            //
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                 //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,          //
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE          //
+// SOFTWARE.                                                                              //
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//------------------------------------------------------------------------------------------
+// Set type of chart
+//------------------------------------------------------------------------------------------
+opencharts.pie = function(){
+    "use strict";
+
+    this._type = "pie";
+    return this;
+};
+
+//------------------------------------------------------------------------------------------
+// Set the data for the current pie chart
+//------------------------------------------------------------------------------------------
+opencharts.pie().data =  function(data){
+    "use strict";
+
+    this._data = data;
+    // Data consistency test missing
+    return this;
+};
+
+//------------------------------------------------------------------------------------------
+// Create pie chart
+//------------------------------------------------------------------------------------------
+opencharts.pie().create = function(){
+    "use strict";
+
+    console.log("creating");
+
+    
+    var data = this._data;
+    var values = data.values;
+    var labels = data.labels;
+    var w = 400;
+    var h = 400;
+    var r = h/2;
+    var color = d3.scale.category20c();
+    
+    if (values.length !== labels.length) {
+        return;
+    }
+
+
+    var vis = d3.select(this._selector).append("svg:svg").data([values]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
+    var pie = d3.layout.pie().value(function(d){
+        return d;
+    });
+
+    // declare an arc generator function
+    var arc = d3.svg.arc().outerRadius(r);
+
+    // select paths, use arc generator to draw
+    var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+    arcs.append("svg:path")
+        .attr("fill", function(d, i){
+            return color(i);
+        })
+        .attr("d", function (d) {
+            // log the result of the arc generator to show how cool it is :)
+            return arc(d);
+        });
+
+    // add the text
+    arcs.append("svg:text").attr("transform", function(d){
+                d.innerRadius = 0;
+                d.outerRadius = r;
+        return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+            return labels[i];
+        }
+    );
+
+
+    return this;
+};
